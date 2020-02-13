ca672bd17de03a6445579297600cb671931c6ca8
Estragna <48624456+Retsam101@users.noreply.github.com>
2020-02-13 23:47:49 +0700

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,4 +1,18 @@
-
+#define MAX_BYTE 256 
+#define SECTOR_SIZE 512 
+#define MAX_FILES 16 
+#define MAX_FILENAME 12 
+#define MAX_SECTORS 20 
+#define DIR_ENTRY_LENGTH 32 
+#define MAP_SECTOR 1 
+#define DIR_SECTOR 2 
+#define TRUE 1
+#define FALSE 0
+#define INSUFFICIENT_SECTORS 0 
+#define NOT_FOUND -1
+#define INSUFFICIENT_DIR_ENTRIES -1 
+#define EMPTY 0x00
+#define USED 0xFF
 
 /* Ini deklarasi fungsi */
 void handleInterrupt21 (int AX, int BX, int CX, int DX);
@@ -103,7 +117,35 @@ void writeSector(char *buffer, int sector){
 }
 
 void readFile(char *buffer, char *filename, int *success){
+	char dir[SECTOR_SIZE];
+	int i, found, j;
 
+
+	readSector(dir, DIR_SECTOR);
+	for(i = 0; i < SECTOR_SIZE; i += DIR_ENTRY_LENGTH) {
+		found = TRUE;
+		for(j = 0; j < MAX_FILENAME; j++) {
+			if(dir[i + j] == '\0' && filename[j] == '\0') {
+				break;
+			}
+			if(dir[i + j] != filename[j]) {
+				found = FALSE;
+				break;
+			}
+		}
+
+		if(found) {
+			j = i + MAX_FILENAME;
+			i = 0;
+			while(i < MAX_SECTORS && dir[i + j] != 0) {
+				readSector(buffer + i * SECTOR_SIZE, dir[j + i]);
+				i++;
+			}
+			*success = TRUE;
+			return;	
+		}
+	}
+	*success = found;
 }
 
 void clear(char *buffer, int length){
@@ -114,7 +156,58 @@ void clear(char *buffer, int length){
 } 
 //Fungsi untuk mengisi buffer dengan 0
 void writeFile(char *buffer, char *filename, int *sectors){
+	char map[SECTOR_SIZE];
+	char dir[SECTOR_SIZE];
+	char sectorBuffer[SECTOR_SIZE]; 
+	int dirIndex;
 
+	readSector(map, MAP_SECTOR); 
+	readSector(dir, DIR_SECTOR);
+	for (dirIndex = 0; dirIndex < MAX_FILES; ++dirIndex) { 
+		if (dir[dirIndex * DIR_ENTRY_LENGTH] == '\0') {
+			break; 
+		}
+	}
+
+	if (dirIndex < MAX_FILES) {
+		int i, j, sectorCount;
+		for (i = 0, sectorCount = 0; i < MAX_BYTE && sectorCount < *sectors; ++i) {
+			if (map[i] == EMPTY) {
+				++sectorCount; 
+			}
+		}
+
+		if (sectorCount < *sectors) {
+			*sectors = INSUFFICIENT_SECTORS;
+			return; 
+		} else {
+			clear(dir + dirIndex * DIR_ENTRY_LENGTH, DIR_ENTRY_LENGTH); 
+			for (i = 0; i < MAX_FILENAME; ++i) {
+				if (filename[i] != '\0') {
+					dir[dirIndex * DIR_ENTRY_LENGTH + i] = filename[i];
+				} else {
+					break; 
+				}
+			}
+			for (i = 0, sectorCount = 0; i < MAX_BYTE && sectorCount < *sectors; ++i) {
+				if (map[i] == EMPTY) {
+					map[i] = USED;
+					dir[dirIndex * DIR_ENTRY_LENGTH + MAX_FILENAME + sectorCount] = i;
+					clear(sectorBuffer, SECTOR_SIZE); 
+					for (j = 0; j < SECTOR_SIZE; ++j) {
+						sectorBuffer[j] = buffer[sectorCount * SECTOR_SIZE + j]; 
+					}
+					writeSector(sectorBuffer, i);
+					++sectorCount; 
+				}
+			} 
+		}
+	} else {
+		*sectors = INSUFFICIENT_DIR_ENTRIES;
+		return; 
+	}
+	writeSector(map, MAP_SECTOR); 
+	writeSector(dir, DIR_SECTOR);
 }
 
 void executeProgram(char *filename, int segment, int *success){
@@ -122,4 +215,15 @@ void executeProgram(char *filename, int segment, int *success){
 // Buka file dengan fungsi readFile
 // Jika sukses, gunakan loop untuk menyalin tiap byte kode dengan fungsi putInMemory(segment, i, buffer[i])​
 // Panggil fungsi launchProgram(segment) (fungsi ini telah didefinisikan di kernel.asm) untuk menjalankan program di segment yang telah terisi
+  char buffer[20 * 512];
+  int i;
+
+  readFile(buffer, filename, success);
+
+  if(*success == 1) {
+    for(i = 0; i < (20 * 512); i++) {
+      putInMemory(segment, i, buffer[i]);
+    }
+    launchProgram(segment);
+  }
 }
