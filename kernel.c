700e6cc4e6f4d4cbdfdee2d792ef5f8a5351ce67
Estragna <48624456+Retsam101@users.noreply.github.com>
2020-02-14 17:38:36 +0700

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,4 +1,4 @@
-#define MAX_BYTE 256 
+#define MAX_BYTE 256
 #define SECTOR_SIZE 512 
 #define MAX_FILES 16 
 #define MAX_FILENAME 12 
@@ -10,7 +10,7 @@
 #define INSUFFICIENT_DIR_ENTRIES -1 
 
 /* Ini deklarasi fungsi */
-void handleInterrupt21 (int AX, int BX, int CX, int DX);
+// void handleInterrupt21(int AX, int BX, int CX, int DX);
 void printString(char *string);
 void readString(char *string);
 void readSector(char *buffer, int sector);
@@ -22,13 +22,39 @@ void executeProgram(char *filename, int segment, int *success);
 int div(int p,int q);
 int mod(int p,int q);
 
-int main() {
+int main () {
+  // putInMemory(0xB000, 0x8000, 'H');
+  // putInMemory(0xB000, 0x8001, 0xD);
+  // putInMemory(0xB000, 0x8002, 'a');
+  // putInMemory(0xB000, 0x8003, 0xD);
+  // putInMemory(0xB000, 0x8004, 'i');
+  // putInMemory(0xB000, 0x8005, 0xD);
+  // makeInterrupt21();
+  // interrupt(0x21, 0x0, "hahahaha", 0, 0);
+  // interrupt(0x21, 0x1, buf, 0, 0);
+  // interrupt(0x21, 0x0, "buffer content: ", 0, 0);
+  // interrupt(0x21, 0x0, buf, 0, 0);
+
+  // executeProgram("milestone1", 0x2000);
+  // while(1);
+  // char buf[SECTOR_SIZE * MAX_SECTORS];
+  // int succ;
   makeInterrupt21();
+  printString("KITA ADALAH");
+
+
+  // interrupt(0x21, 0x4, buf, "key.txt", &succ);
+  // if(succ) {
+  //   interrupt(0x21, 0x0, buf, 0, 0);
+  // } else {
+  //   interrupt(0x21, 0x6, "keyproc", 0x2000, &succ);
+  // }
   while (1);
 }
 
-void handleInterrupt21 (int AX, int BX, int CX, int DX){
-  switch (AX) {
+
+void handleInterrupt21(int AX, int BX, int CX, int DX){
+  switch(AX){
     case 0x0:
       printString(BX);
       break;
@@ -112,35 +138,35 @@ void writeSector(char *buffer, int sector){
 }
 
 void readFile(char *buffer, char *filename, int *success){
-	char dir[SECTOR_SIZE];
-	int i, found, j;
+  char dir[SECTOR_SIZE];
+  int i, found, j;
 
 
-	readSector(dir, DIR_SECTOR);
-	for(i = 0; i < SECTOR_SIZE; i += DIR_ENTRY_LENGTH) {
-		found = 1;
-		for(j = 0; j < MAX_FILENAME; j++) {
-			if(dir[i + j] == '\0' && filename[j] == '\0') {
-				break;
-			}
-			if(dir[i + j] != filename[j]) {
-				found = 0;
-				break;
-			}
-		}
+  readSector(dir, DIR_SECTOR);
+  for(i = 0; i < SECTOR_SIZE; i += DIR_ENTRY_LENGTH) {
+    found = 1;
+    for(j = 0; j < MAX_FILENAME; j++) {
+      if(dir[i + j] == '\0' && filename[j] == '\0') {
+        break;
+      }
+      if(dir[i + j] != filename[j]) {
+        found = 0;
+        break;
+      }
+    }
 
-		if(found) {
-			j = i + MAX_FILENAME;
-			i = 0;
-			while(i < MAX_SECTORS && dir[i + j] != 0) {
-				readSector(buffer + i * SECTOR_SIZE, dir[j + i]);
-				i++;
-			}
-			*success = 1;
-			return;	
-		}
-	}
-	*success = found;
+    if(found) {
+      j = i + MAX_FILENAME;
+      i = 0;
+      while(i < MAX_SECTORS && dir[i + j] != 0) {
+        readSector(buffer + i * SECTOR_SIZE, dir[j + i]);
+        i++;
+      }
+      *success = 1;
+      return; 
+    }
+  }
+  *success = found;
 }
 
 void clear(char *buffer, int length){
@@ -151,58 +177,58 @@ void clear(char *buffer, int length){
 } 
 //Fungsi untuk mengisi buffer dengan 0
 void writeFile(char *buffer, char *filename, int *sectors){
-	char map[SECTOR_SIZE];
-	char dir[SECTOR_SIZE];
-	char sectorBuffer[SECTOR_SIZE]; 
-	int dirIndex;
+  char map[SECTOR_SIZE];
+  char dir[SECTOR_SIZE];
+  char sectorBuffer[SECTOR_SIZE]; 
+  int dirIndex;
 
-	readSector(map, MAP_SECTOR); 
-	readSector(dir, DIR_SECTOR);
-	for (dirIndex = 0; dirIndex < MAX_FILES; ++dirIndex) { 
-		if (dir[dirIndex * DIR_ENTRY_LENGTH] == '\0') {
-			break; 
-		}
-	}
+  readSector(map, MAP_SECTOR); 
+  readSector(dir, DIR_SECTOR);
+  for (dirIndex = 0; dirIndex < MAX_FILES; ++dirIndex) { 
+    if (dir[dirIndex * DIR_ENTRY_LENGTH] == '\0') {
+      break; 
+    }
+  }
 
-	if (dirIndex < MAX_FILES) {
-		int i, j, sectorCount;
-		for (i = 0, sectorCount = 0; i < MAX_BYTE && sectorCount < *sectors; ++i) {
-			if (map[i] == 0x00) {
-				++sectorCount; 
-			}
-		}
+  if (dirIndex < MAX_FILES) {
+    int i, j, sectorCount;
+    for (i = 0, sectorCount = 0; i < MAX_BYTE && sectorCount < *sectors; ++i) {
+      if (map[i] == 0x00) {
+        ++sectorCount; 
+      }
+    }
 
-		if (sectorCount < *sectors) {
-			*sectors = INSUFFICIENT_SECTORS;
-			return; 
-		} else {
-			clear(dir + dirIndex * DIR_ENTRY_LENGTH, DIR_ENTRY_LENGTH); 
-			for (i = 0; i < MAX_FILENAME; ++i) {
-				if (filename[i] != '\0') {
-					dir[dirIndex * DIR_ENTRY_LENGTH + i] = filename[i];
-				} else {
-					break; 
-				}
-			}
-			for (i = 0, sectorCount = 0; i < MAX_BYTE && sectorCount < *sectors; ++i) {
-				if (map[i] == 0x00) {
-					map[i] = 0xFF;
-					dir[dirIndex * DIR_ENTRY_LENGTH + MAX_FILENAME + sectorCount] = i;
-					clear(sectorBuffer, SECTOR_SIZE); 
-					for (j = 0; j < SECTOR_SIZE; ++j) {
-						sectorBuffer[j] = buffer[sectorCount * SECTOR_SIZE + j]; 
-					}
-					writeSector(sectorBuffer, i);
-					++sectorCount; 
-				}
-			} 
-		}
-	} else {
-		*sectors = INSUFFICIENT_DIR_ENTRIES;
-		return; 
-	}
-	writeSector(map, MAP_SECTOR); 
-	writeSector(dir, DIR_SECTOR);
+    if (sectorCount < *sectors) {
+      *sectors = INSUFFICIENT_SECTORS;
+      return; 
+    } else {
+      clear(dir + dirIndex * DIR_ENTRY_LENGTH, DIR_ENTRY_LENGTH); 
+      for (i = 0; i < MAX_FILENAME; ++i) {
+        if (filename[i] != '\0') {
+          dir[dirIndex * DIR_ENTRY_LENGTH + i] = filename[i];
+        } else {
+          break; 
+        }
+      }
+      for (i = 0, sectorCount = 0; i < MAX_BYTE && sectorCount < *sectors; ++i) {
+        if (map[i] == 0x00) {
+          map[i] = 0xFF;
+          dir[dirIndex * DIR_ENTRY_LENGTH + MAX_FILENAME + sectorCount] = i;
+          clear(sectorBuffer, SECTOR_SIZE); 
+          for (j = 0; j < SECTOR_SIZE; ++j) {
+            sectorBuffer[j] = buffer[sectorCount * SECTOR_SIZE + j]; 
+          }
+          writeSector(sectorBuffer, i);
+          ++sectorCount; 
+        }
+      } 
+    }
+  } else {
+    *sectors = INSUFFICIENT_DIR_ENTRIES;
+    return; 
+  }
+  writeSector(map, MAP_SECTOR); 
+  writeSector(dir, DIR_SECTOR);
 }
 
 void executeProgram(char *filename, int segment, int *success){
